9725b8c876740be3c6c40fd1e024d865abf91e9f
eabra <emab21@student.bth.se>
2024-02-28 14:57:29 +0100

    Finished App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
+import Container from 'react-bootstrap/Container'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,107 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+  isOpen: boolean;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  function getItemQuantity(id: number): number {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number): void {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id: number): void {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number): void {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  }
+
+  const value = {
+    openCart: openCart,
+    closeCart: closeCart,
+    getItemQuantity: getItemQuantity,
+    increaseCartQuantity: increaseCartQuantity,
+    decreaseCartQuantity: decreaseCartQuantity,
+    removeFromCart: removeFromCart,
+    cartItems: cartItems,
+    cartQuantity: cartQuantity,
+    isOpen: isOpen,
+  };
+
+  return (
+    <ShoppingCartContext.Provider value={value}>
+      {children}
+      <ShoppingCart isOpen={isOpen} />
+    </ShoppingCartContext.Provider>
+  );
+}
